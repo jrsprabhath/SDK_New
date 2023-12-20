968554428f61416d87733fb275bcdafebf0a273a
rushi.jagarlamudi <jrsprabhath@gmail.com>
2023-12-20 15:37:17 +0530

    MavelLocal Push

--- SDK_New/build.gradle.kts
+++ SDK_New/build.gradle.kts
@@ -45,3 +45,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "com.example"  // Replace with your group ID
+            artifactId = "SDK_New"  // Replace with your artifact ID
+            version = "1.0.0"  // Replace with your version
+        }
+    }
+
+    repositories {
+        mavenLocal()
+    }
+}
