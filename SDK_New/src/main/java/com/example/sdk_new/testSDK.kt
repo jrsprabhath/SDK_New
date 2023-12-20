ecf2012b3be9a7df007ef3b9fd9eb17740be1348
rushi.jagarlamudi <jrsprabhath@gmail.com>
2023-12-20 16:33:52 +0530

    Mavel Push

--- SDK_New/src/main/java/com/example/sdk_new/testSDK.kt
+++ SDK_New/src/main/java/com/example/sdk_new/testSDK.kt
@@ -3,25 +3,8 @@ package com.example.sdk_new
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.sdk_new.Adapter.Adapter
-import com.example.sdk_new.Logic.DeleteUser
 import com.example.sdk_new.Logic.GetSurveys
-import com.example.sdk_new.Logic.UpdateUser
-import com.example.sdk_new.Models.CreateUser
-import com.example.sdk_new.Models.Survey
-import com.example.sdk_new.Service.RetrofitClient
-import com.google.gson.Gson
-import kotlinx.coroutines.awaitAll
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class testSDK():AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +13,7 @@ class testSDK():AppCompatActivity(){
     companion object{
         fun GetSurveys(context: Context, userGuid: String): Intent {
             return Intent(context, GetSurveys::class.java).apply {
-                putExtra(GetSurveys.USER_GUID_EXTRA, userGuid)
+                putExtra("user_guid", userGuid)
             }
         }
         suspend fun CreateUser( user: com.example.sdk_new.Models.CreateUser):String{
